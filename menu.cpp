a0722f8a119ada730eb15648d0b19c8ddcf6c1e5
edu4rdolinux <esbarros@proton.me>
2025-06-07 14:10:04 -0300

    Choice to option and switch test

--- menu.cpp
+++ menu.cpp
@@ -2,9 +2,9 @@
 #include "globals.h"
 
 void menu() {
-    int choice;
+    int option;
 
-    const char* choices[] = {
+    const char* options[] = {
         "Register a student",
         "Update a student",
         "Delete a student",
@@ -15,13 +15,33 @@ void menu() {
     std::cout << "\nMenu options:\n\n";
 
     for (int i = 0; i < 5; i++) {
-        std::cout << (i + 1) << ". " << choices[i] << "\n";
+        std::cout << (i + 1) << ". " << options[i] << "\n";
     }
 
     std::cout << "\n";
 
-    std::cin >> choice;
-    std::cout << "You selected option " << choice << std::endl;
+    std::cout << "\nChoose an option: ";
+    std::cin >> option;
+
+    switch (option) {
+        case 1:
+            printf(options[0]);
+            break;
+        case 2:
+            printf(options[1]);
+            break;
+        case 3:
+            printf(options[2]);
+            break;
+        case 4:
+            printf(options[3]);
+            break;
+        case 5:
+            printf(options[4]);
+            break;
+        default:
+            printf("Invalid option");
+    }
 
     studentsCounter = 0;
 }
